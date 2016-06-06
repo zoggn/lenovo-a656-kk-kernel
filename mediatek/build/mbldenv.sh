6118a2cb627b3b7042d96532e8f2c090b996f021
pavel <shlyak-pavel2011@ya.ru>
2016-06-06 23:07:07 +0400

    Added toolchain

--- mediatek/build/mbldenv.sh
+++ mediatek/build/mbldenv.sh
@@ -2,13 +2,14 @@
 # ##########################################################
 # ALPS(Android4.1 based) build environment profile setting
 # ##########################################################
-# Overwrite JAVA_HOME environment variable setting if already exists
-JAVA_HOME=/opt/jdk1.6.0_26
-export JAVA_HOME
-
 # Overwrite ANDROID_JAVA_HOME environment variable setting if already exists
-ANDROID_JAVA_HOME=/opt/jdk1.6.0_26
+ANDROID_JAVA_HOME=$JAVA_HOME
 export ANDROID_JAVA_HOME
+
+# Overwrite PATH environment setting for JDK & arm-eabi if already exists
+PATH=$JAVA_HOME/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PATH
+export PATH
+
 
 # Overwrite PATH environment setting for JDK & arm-eabi if already exists
 PATH=/opt/jdk1.6.0_26/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PWD/prebuilts/misc/linux-x86/make:$PATH
